6071bb9e13eb0719c9fa3b3f2f44edc245931c35
Stephen Wang <holleratme420@gmail.com>
2023-10-19 06:33:33 -0700

    added the first boilerplate folder setup

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Vin Doctor Home</Text>
+        </View>
+    );
+};
+
+export default Home;
